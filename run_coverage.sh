7be4386fd9e7c50615a8b3fcc849c85024756edb
Eric Kerfoot <eric.kerfoot@gmail.com>
2025-06-08 23:06:38 +0000

    Update to script, still trying to get to work well
    Signed-off-by: Eric Kerfoot <eric.kerfoot@gmail.com>

--- run_coverage.sh
+++ run_coverage.sh
@@ -5,9 +5,9 @@
 # pip uninstall monai -y
 
 export CUDA_VISIBLE_DEVICES=0,1
+export MAX_JOBS=64
 
-
-rm -rf .eggs monai.egg-info *.zarr .coverage build cufile.log runner.log
+rm -rf .eggs monai.egg-info *.zarr .coverage cufile.log runner.log
 
 # BUILD_MONAI=1 ./runtests.sh --build --coverage --unittests --disttests
 # BUILD_MONAI=1 ./runtests.sh --build --coverage --net 
@@ -16,7 +16,8 @@ rm -rf .eggs monai.egg-info *.zarr .coverage build cufile.log runner.log
 
 # coverage combine --append .coverage
 
-IMG=nvcr.io/nvidia/pytorch:24.10-py3
+# IMG=nvcr.io/nvidia/pytorch:24.10-py3
+IMG=monai
 
 (
 docker run -i --rm --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
@@ -25,19 +26,22 @@ docker run -i --rm --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
     pwd
     nvidia-smi
 
-    export DEBIAN_FRONTEND=noninteractive
-    apt-get update 
-    apt-get install -y libopenslide0 curl git zip
+    # export DEBIAN_FRONTEND=noninteractive
+    # apt-get update 
+    # apt-get install -y libopenslide0 curl git zip
 
-    python -m pip install --upgrade pip
-
-    pip install -r requirements-dev.txt
-    pip install .[all]
+    # python -m pip install --upgrade pip
+    # pip uninstall monai -y
+    # pip install -r requirements-dev.txt
+    # pip install .[all]
     
-    BUILD_MONAI=1 ./runtests.sh --build --coverage --unittests 
-    #--disttests --net
+    BUILD_MONAI=1 ./runtests.sh --build --coverage --unittests --net
+    # --disttests 
+    BUILD_MONAI=1 ./runtests.sh --build --coverage --unittests --disttests 
+    BUILD_MONAI=1 ./runtests.sh --build --coverage --net
     coverage xml --ignore-errors
     
+    zip -r $(date '+coverage_%Y%m%d_%H%M%S.zip') .coverage coverage.xml
     chown -R $(id -u):$(id -g) .
 _EOF_
 
