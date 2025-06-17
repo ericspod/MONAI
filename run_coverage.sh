1f5cd392c9d381f5b34ee775860f28498004370f
Eric Kerfoot <eric.kerfoot@gmail.com>
2025-06-17 12:43:50 +0100

    Update to run_coverage.sh
    Signed-off-by: Eric Kerfoot <eric.kerfoot@gmail.com>

--- run_coverage.sh
+++ run_coverage.sh
@@ -19,29 +19,37 @@ rm -rf .eggs monai.egg-info *.zarr .coverage cufile.log runner.log
 # IMG=nvcr.io/nvidia/pytorch:24.10-py3
 IMG=monai
 
-(
-docker run -i --rm --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
-        -v $(pwd):/monai -w /monai --gpus '"device=0,1"' $IMG /bin/bash -s << _EOF_
 
+run_script=$(date '+/tmp/coverage_%s.py')
+timestamp=$(date '+coverage_%Y%m%d_%H%M%S')
+
+cat - > $run_script << _EOF_
+(
     pwd
     nvidia-smi
 
-    # export DEBIAN_FRONTEND=noninteractive
-    # apt-get update 
-    # apt-get install -y libopenslide0 curl git zip
+    export DEBIAN_FRONTEND=noninteractive
+    apt-get update 
+    apt-get install -y libopenslide0 curl git zip
 
-    # python -m pip install --upgrade pip
-    # pip uninstall monai -y
-    # pip install -r requirements-dev.txt
-    # pip install .[all]
+    python -m pip install --upgrade pip
+    pip uninstall monai -y
+    pip install -r requirements-dev.txt
+    pip install .[all]
     
     #BUILD_MONAI=1 ./runtests.sh --build --coverage --unittests --net --disttests 
     BUILD_MONAI=1 ./runtests.sh --build --coverage --unittests 
     BUILD_MONAI=1 ./runtests.sh --build --coverage --net
+    BUILD_MONAI=1 ./runtests.sh --build --coverage --disttests
     coverage xml --ignore-errors
     
-    zip -r $(date '+coverage_%Y%m%d_%H%M%S.zip') .coverage coverage.xml
+    zip -r ${timestamp}.zip .coverage coverage.xml
     chown -R $(id -u):$(id -g) .
+) 2>&1 | tee ${timestamp}.log
 _EOF_
 
-) 2>&1 | tee run_coverage.log
+
+docker run -d --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
+        -v $(pwd):/monai -v /tmp:/tmp -w /monai --gpus '"device=0,1"' $IMG \
+        /bin/bash $run_script 
+        #2>&1 | tee run_coverage.log
