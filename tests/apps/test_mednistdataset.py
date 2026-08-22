3a9b072d346c6be861a4000d17c1cd6499ceb283
Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
2026-08-22 22:10:59 +0100

    Update tests
    Signed-off-by: Eric Kerfoot <17726042+ericspod@users.noreply.github.com>

--- tests/apps/test_mednistdataset.py
+++ tests/apps/test_mednistdataset.py
@@ -11,32 +11,46 @@
 
 from __future__ import annotations
 
-import os
 import shutil
 import unittest
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
-from monai.apps import MedNISTDataset
+from monai.apps import MedNISTDataset, logger
 from monai.data import MetaTensor
-from monai.transforms import Compose, EnsureChannelFirstd, LoadImaged, ScaleIntensityd
+from monai.transforms import Compose, LoadImaged, ScaleIntensityd
 from tests.test_utils import skip_if_downloading_fails, skip_if_quick
 
 MEDNIST_FULL_DATASET_LENGTH = 58954
 
 
 class TestMedNISTDataset(unittest.TestCase):
-    @skip_if_quick
-    def test_values(self):
-        testing_dir = Path(__file__).parents[1] / "testing_data"
-        transform = Compose(
-            [
-                LoadImaged(keys="image"),
-                EnsureChannelFirstd(keys="image", channel_dim="no_channel"),
-                ScaleIntensityd(keys="image"),
-            ]
-        )
+    def test_need_download(self):
+        """Test that the dataset raises RuntimeError if the data isn't downloaded yet and `download` is False."""
+        with TemporaryDirectory() as tdir, self.assertRaisesRegex(RuntimeError, "^Cannot find dataset directory"):
+            MedNISTDataset(root_dir=tdir, section="test", download=False)
 
-        def _test_dataset(dataset):
+    def test_rootdir_not_dir(self):
+        root_dir = Path("./NONEXISTENT")
+        self.assertFalse(root_dir.is_dir())
+        with self.assertRaisesRegex(ValueError, "Root directory root_dir must be a directory."):
+            MedNISTDataset(root_dir=root_dir, section="test", download=False)
+
+    @skip_if_quick
+    def test_dataset_contents(self):
+        """
+        Test the dataset with an initial downloading of the tarball, followed by tests in subtests. This simplifies
+        testing by downloading the data once and testing with it multiple ways in one simpler method.
+        """
+        testing_dir = Path(__file__).parents[1] / "testing_data"
+        transform = Compose([LoadImaged(keys="image", ensure_channel_first=True), ScaleIntensityd(keys="image")])
+
+        # common set of arguments for all dataset instantiations below
+        common_args = dict(root_dir=testing_dir, section="test", progress=False)
+
+        def _test_dataset(dataset: MedNISTDataset):
+            self.assertEqual(dataset.get_num_classes(), 6)
+            self.assertTupleEqual(dataset.get_classes(), ("AbdomenCT", "BreastMRI", "CXR", "ChestCT", "Hand", "HeadCT"))
             self.assertEqual(len(dataset), int(MEDNIST_FULL_DATASET_LENGTH * dataset.test_frac))
             self.assertTrue("image" in dataset[0])
             self.assertTrue("label" in dataset[0])
@@ -44,28 +58,29 @@ class TestMedNISTDataset(unittest.TestCase):
             self.assertTupleEqual(dataset[0]["image"].shape, (1, 64, 64))
 
         with skip_if_downloading_fails():
-            data = MedNISTDataset(
-                root_dir=testing_dir, transform=transform, section="test", download=True, copy_cache=False
-            )
+            try:
+                logger.disabled = True  # silence logging
+                data = MedNISTDataset(transform=transform, download=True, copy_cache=False, **common_args)
+                self.addCleanup(shutil.rmtree, str(testing_dir / "MedNIST"))  # cleanup expanded data when finished
+            finally:
+                logger.disabled = False
 
-        _test_dataset(data)
+        with self.subTest("Test downloaded dataset properties"):
+            _test_dataset(data)
 
-        # testing from
-        data = MedNISTDataset(
-            root_dir=Path(testing_dir), transform=transform, section="test", download=False, runtime_cache=True
-        )
-        self.assertEqual(data.get_num_classes(), 6)
-        _test_dataset(data)
-        data = MedNISTDataset(root_dir=testing_dir, section="test", download=False)
-        self.assertTupleEqual(data[0]["image"].shape, (64, 64))
-        # test same dataset length with different random seed
-        data = MedNISTDataset(root_dir=testing_dir, transform=transform, section="test", download=False, seed=42)
-        _test_dataset(data)
-        self.assertEqual(data[0]["class_name"], "AbdomenCT")
-        self.assertEqual(data[0]["label"], 0)
-        shutil.rmtree(os.path.join(testing_dir, "MedNIST"))
-        with self.assertRaisesRegex(RuntimeError, "^Cannot find dataset directory"):
-            MedNISTDataset(root_dir=testing_dir, transform=transform, section="test", download=False)
+        with self.subTest("Test non-downloaded dataset properties"):
+            data = MedNISTDataset(transform=transform, download=False, runtime_cache=True, **common_args)
+            _test_dataset(data)
+
+        with self.subTest("Test dataset without transforms"):
+            data = MedNISTDataset(download=False, **common_args)
+            self.assertTupleEqual(data[0]["image"].shape, (64, 64))
+
+        with self.subTest("Test with different random seed"):
+            data = MedNISTDataset(transform=transform, download=False, seed=42, **common_args)
+            _test_dataset(data)
+            self.assertEqual(data[0]["class_name"], "AbdomenCT")
+            self.assertEqual(data[0]["label"], 0)
 
 
 if __name__ == "__main__":
